8ea7615f52c3f2895943fd90e1f1b3c6a74e17e9
Oleg <oleg.v.avdeev@gmail.com>
2008-10-29 22:29:01 +0300

    layout changed

--- src/com/ajaxie/lastfm/R.java
+++ src/com/ajaxie/lastfm/R.java
@@ -20,22 +20,22 @@ public final class R {
         public static final int stop=0x7f020006;
     }
     public static final class id {
-        public static final int album_name_text=0x7f050007;
-        public static final int album_view=0x7f050005;
+        public static final int album_name_text=0x7f050004;
+        public static final int album_view=0x7f050002;
         public static final int ban_button=0x7f05000b;
         public static final int cancel_button=0x7f05000f;
-        public static final int creator_name_text=0x7f050006;
+        public static final int creator_name_text=0x7f050003;
         public static final int love_button=0x7f05000a;
         public static final int ok_button=0x7f050010;
         public static final int password=0x7f05000e;
-        public static final int play_button=0x7f050002;
+        public static final int play_button=0x7f050007;
         public static final int settings_button=0x7f05000c;
-        public static final int skip_button=0x7f050004;
+        public static final int skip_button=0x7f050009;
         public static final int station_url=0x7f050001;
         public static final int status_text=0x7f050000;
-        public static final int stop_button=0x7f050003;
-        public static final int time_counter=0x7f050009;
-        public static final int track_name_text=0x7f050008;
+        public static final int stop_button=0x7f050008;
+        public static final int time_counter=0x7f050006;
+        public static final int track_name_text=0x7f050005;
         public static final int username=0x7f05000d;
     }
     public static final class layout {
